b94e96af9343aa5710c71400989b3da04720788e
Milarc970 <jaime@milarc.com>
2025-04-11 14:18:22 -0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-// Minor update to trigger Vercel build
+// Minor update to trigger Vercel deployment
 export default async function handler(req, res) {
   const response = await fetch("https://sheetdb.io/api/v1/ig2o5huoe0dew");
   const data = await response.json();
